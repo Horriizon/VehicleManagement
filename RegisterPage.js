d54b0451d34423232c32695ce1f024431980c649
Horriizon <142866113+Horriizon@users.noreply.github.com>
2023-11-23 14:06:07 +0600

    Create RegisterPage.js

---
+++ RegisterPage.js
@@ -0,0 +1,33 @@
+// RegisterPage.js
+
+import React, { useState } from 'react';
+
+const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    // Add user registration logic here
+    // For simplicity, we're just displaying the entered username and password
+    alert(`Registered with Username: ${username} and Password: ${password}`);
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+};
+
+export default RegisterPage;
